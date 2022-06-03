80758a2214e9d324a1be80ae7d1c68c296b95932
PhpenixPythonCode <58854416+PhpenixPythonCode@users.noreply.github.com>
2022-06-03 10:32:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ var port = 3001
 const app = express()
 
 app.get('/', async (req,res) => {
-    const {url} = req.query
-
+    var {url} = req.query
+    
     if(!url){
         return res.send('No url provided')
     }else{
+        if(url.startsWith('http')) url = url.replace(/^(http:\/\/\.|https:\/\/\.|http:\/\/|https:\/\/)/, '')
+        console.log(url)
         try{
             const browser = await puppeteer.launch()
             const page = await browser.newPage()
+            
             await page.goto(`https://${url}`)
 
             let document = await page.evaluate(() => document.documentElement.outerHTML)
-            document = absolutify(document, `/?url=${url.split('/')[0]}`)
+            if(!document || document == {}) return res.send('404')
+            document = absolutify(document, `https://localhost:${port}/?url=${url.split('/')[0]}`)
+            // document = urlify(document)
             browser.close()
             return res.send(document)
         }catch(err){
@@ -26,6 +31,13 @@ app.get('/', async (req,res) => {
         
     }
 })
+
+function urlify(text) {
+    var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    return text.replace(urlRegex, function(url) {
+        return '/?url='+url;
+    })
+}
 
 app.listen(port, () => {
     console.log('proxy server running! http://localhost:' + port)
